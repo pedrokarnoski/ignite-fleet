2daa737e7b1cf36251e7956e8ee8b93dd081a896
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-07-04 22:02:50 -0300

    fix: Status do veículo

--- src/components/CarStatus.tsx
+++ src/components/CarStatus.tsx
@@ -15,16 +15,16 @@ type CarStatusProps = TouchableOpacityProps & {
 export function CarStatus({ licensePlate, ...rest }: CarStatusProps) {
   return (
     <TouchableOpacity {...rest} activeOpacity={0.7}>
-      <View className="w-full flex-row items-center my-8 p-8 rounded-md bg-gray-700">
+      <View className="w-full flex-row items-center my-8 p-4 rounded-md bg-gray-700">
         <View className="w-20 h-20 rounded-md bg-gray-600 mr-3 justify-center items-center">
           <Ionicons
             name={licensePlate ? "car-outline" : "key-outline"}
-            size={32}
+            size={48}
             color={colors["brand-light"]}
           />
         </View>
 
-        <Text className="flex-1 text-gray-100">
+        <Text className="flex-1 pl-2 text-gray-100">
           {licensePlate
             ? `Veículo ${licensePlate} em uso. `
             : "Nenhum veículo em uso. "}
