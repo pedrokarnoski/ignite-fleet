8dfbc5207f96235ba414b8a61c1a76b8d5ec2d08
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-07-08 21:24:35 -0300

    chore: Api maps

---
+++ app.config.js
@@ -0,0 +1,50 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+module.exports = {
+  expo: {
+    name: "ignite-fleet",
+    slug: "ignite-fleet",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "dark",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "cover",
+      backgroundColor: "#202024",
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.kns.ignitefleet",
+      config: {
+        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#202024",
+      },
+      package: "com.kns.ignitefleet",
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+        },
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: [
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission:
+            "Allow $(PRODUCT_NAME) to use your location.",
+        },
+      ],
+    ],
+  },
+};
